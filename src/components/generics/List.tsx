ff13dc9cd4b25df966f27fc30deaadd26e5ee43f
Chetan Thakur <80780935+Amcelo13@users.noreply.github.com>
2023-08-18 13:36:01 +0530

    Also this way of React.FC

--- src/components/generics/List.tsx
+++ src/components/generics/List.tsx
@@ -17,3 +17,29 @@ export const List = <T extends { id: number }>({ items, onClick }: ListProps<T>)
         </div>
     );
 };
+
+    ############################  Also this way of React.FC##########################################
+ import React from "react";
+
+type ListProps<T> = {
+  items: T[];
+  onClick: (value: T) => void;
+};
+
+const List: React.FC<ListProps<any>> = ({ items, onClick }) => {
+  return (
+    <div>
+      <h2>List of items</h2>
+      {items.map((item, i) => {
+        return (
+          <div style={{ cursor: 'pointer' }} key={i} onClick={() => onClick(item)}>
+            {item}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default List;
+
